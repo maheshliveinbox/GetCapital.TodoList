9a3c6668422b8d6a3abd3ade8940ce7dd305fd6c
Kapila Mahesh <Mahesh.Gamage@nexa.com.au>
2021-04-26 17:46:36 +1000

    Add Unit Tests

--- GetCapital.TodoList.Tests/TodoItemsControllerTests.cs
+++ GetCapital.TodoList.Tests/TodoItemsControllerTests.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Threading.Tasks;
 using GetCapital.TodoList.Application.Dtos.TodoItem;
 using GetCapital.TodoList.Application.Services.TodoItem;
@@ -14,7 +15,7 @@ namespace GetCapital.TodoList.Tests
     public class TodoItemsControllerTests
     {
         [Fact]
-        public async Task Get_ListOfTodoItems()
+        public async Task Get_ListOfTodoItems_Should_Return_OK_Response()
         {
             // Arrange
             var mockService = new Mock<ITodoItemService>();
@@ -26,9 +27,89 @@ namespace GetCapital.TodoList.Tests
             var result = await controller.Get();
 
             // Assert
-            var serviceResponse = Assert.IsType<ServiceResponse<List<GetTodoItemsDto>>>(result);
+            var serviceResponse = Assert.IsType<OkObjectResult>(result);
 
-            Assert.Equal(3, serviceResponse.Data.Count);
+            Assert.Equal(200, serviceResponse.StatusCode);
+        }
+
+        [Fact]
+        public async Task Get_OnlyOneTodoItem_Should_Return_OK_Response()
+        {
+            // Arrange
+            var mockService = new Mock<ITodoItemService>();
+            mockService.Setup(m => m.GetAllAsync())
+                .ReturnsAsync(GetTestResponse_GetAll());
+            var controller = new TodoItemsController(mockService.Object);
+            var listItemid = 1;
+
+            // Act
+            var result = await controller.GetItem(listItemid);
+
+            // Assert
+            var serviceResponse = Assert.IsType<OkObjectResult>(result);
+
+            Assert.Equal(200, serviceResponse.StatusCode);
+        }
+
+        [Fact]
+        public async Task AddNewItem_Should_Return_OK_Response()
+        {
+            // Arrange
+            var mockService = new Mock<ITodoItemService>();
+            mockService.Setup(m => m.GetAllAsync())
+                .ReturnsAsync(GetTestResponse_GetAll());
+            var controller = new TodoItemsController(mockService.Object);
+
+            var newItem = new AddTodoItemDto
+            {
+                Name = "New Item"
+            };
+
+            // Act
+            var result = await controller.AddItem(newItem);
+
+            // Assert
+            var serviceResponse = Assert.IsType<OkObjectResult>(result);
+
+            Assert.Equal(200, serviceResponse.StatusCode);
+        }
+
+        [Fact]
+        public async Task MarkAsCompleted_Should_Return_OK_Response()
+        {
+            // Arrange
+            var mockService = new Mock<ITodoItemService>();
+            mockService.Setup(m => m.GetAllAsync())
+                .ReturnsAsync(GetTestResponse_GetAll());
+            var controller = new TodoItemsController(mockService.Object);
+
+            var listItemId = 1;
+
+            // Act
+            var result = await controller.MarkAsCompleted(listItemId);
+
+            // Assert
+            var serviceResponse = Assert.IsType<OkObjectResult>(result);
+            Assert.Equal(200, serviceResponse.StatusCode);
+        }
+
+        [Fact]
+        public async Task DeleteItem_Should_Return_OK_Response()
+        {
+            // Arrange
+            var mockService = new Mock<ITodoItemService>();
+            mockService.Setup(m => m.GetAllAsync())
+                .ReturnsAsync(GetTestResponse_GetAll());
+            var controller = new TodoItemsController(mockService.Object);
+
+            var listItemId = 1;
+
+            // Act
+            var result = await controller.DeleteItem(listItemId);
+            // Assert
+            var serviceResponse = Assert.IsType<OkObjectResult>(result);
+
+            Assert.Equal(200, serviceResponse.StatusCode);
         }
 
         private ServiceResponse<List<GetTodoItemsDto>> GetTestResponse_GetAll()
